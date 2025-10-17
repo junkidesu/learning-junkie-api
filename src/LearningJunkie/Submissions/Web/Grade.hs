945540487160d4cbc7dbdf6726f8b34ea919ad8f
Anwar Sh. <alvaro.sh03@gmail.com>
2025-10-18 04:53:28 +0500

    Restrict grading to course instructors

--- src/LearningJunkie/Submissions/Web/Grade.hs
+++ src/LearningJunkie/Submissions/Web/Grade.hs
@@ -3,10 +3,12 @@
 
 module LearningJunkie.Submissions.Web.Grade where
 
+import Control.Monad (when)
 import Data.Int (Int32)
 import LearningJunkie.Submissions.Database (modifySubmissionGrade, selectSubmissionById, toSubmissionType)
 import LearningJunkie.Submissions.Submission (Submission)
 import LearningJunkie.Submissions.Submission.ManualGrade (ManualGrade)
+import LearningJunkie.Users.Database.Table (PrimaryKey (UserId), UserT (_userId))
 import LearningJunkie.Web.AppM (AppM)
 import qualified LearningJunkie.Web.Auth.User as Auth
 import LearningJunkie.Web.JWTAuth (JWTAuth)
@@ -22,11 +24,20 @@ type API =
         :> PostCreated '[JSON] Submission
 
 handler :: AuthResult Auth.AuthUser -> Int32 -> ManualGrade -> AppM Submission
-handler (Authenticated _authUser) submissionId manualGrade = do
+handler (Authenticated authUser) submissionId manualGrade = do
     mbSubmission <- selectSubmissionById submissionId
 
     case mbSubmission of
         Nothing -> throwError err404
-        Just _submission -> do
+        Just _submission@(_, _, _exercise@(_, _lesson@(_, _course@(_, _, _instructor@(user, _))))) -> do
+            -- we first check if the teacher is allowed to change the submission
+            let
+                userId :: Int32
+                userId = _userId user
+
+            -- Cannot grade if the instructor is not from the course
+            when (Auth.id authUser /= userId) $ throwError err401
+
+            -- first let's check the state of the exercise. If it is success, cannot change manually.
             toSubmissionType <$> modifySubmissionGrade submissionId manualGrade
 handler _ _ _ = throwError err401
